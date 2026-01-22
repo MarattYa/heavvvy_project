aae4791cd3b08bd658de5d2d105eb0e75170fbcb
MarattYa <mrrayman06@gmail.com>
2026-01-22 17:49:45 +0300

    cart

--- heavvvy/settings.py
+++ heavvvy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
